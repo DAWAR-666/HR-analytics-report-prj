c768e78ecc9bbca01a478bb767a8ab1fa59ab725
DAWAR <dawarpratham8@gmail.com>
2025-10-25 00:23:38 +0530

    functions created

--- hr.js
+++ hr.js
@@ -1,22 +1,39 @@
 
-        const dataGiven=document.getElementById('dataGiven');
+        const tablebody=document.getElementById('enter');
+        const total=document.getElementById('total');
+        const bonus=document.getElementById('change');
         async function getdata(){
         let file=await fetch('hr.json');
         let data=await file.json()
    
         return data
     }
+    function addData(data){
+        let text='';
+        data.forEach(element=>{
+            text+='<tr><td>'
+            text+=JSON.stringify(element.id);
+            text+='</td><td>'
+            text+=JSON.stringify(element.name);
+            text+='</td><td>'
+            text+=JSON.stringify(element.department);
+            text+='</td><td>'
+            text+=JSON.stringify(element.salary);
+            text+='</td><td>'
+            text+=JSON.stringify(element.experience);
+            text+='</td></tr>'
+        })
+        tablebody.innerHTML=text;
+    }
     async function display() {
         let data=await getdata();
-        console.log(typeof data)
-        dataGiven.innerText=JSON.stringify(data,null,2);
-        
+        addData(data);
     }
      async function sortdata(){
          let data=await getdata();
          const sortedData=data.toSorted((a,b)=>b.salary-a.salary)
-            console.log(sortedData);
-            return sortedData;
+         addData(sortedData) 
+         return sortedData;  
      }
      function ignore(x) {
         return x.experience>=3;
@@ -25,19 +42,32 @@
      async function summary() {
         let data=await sortdata();
         filterData=data.filter(ignore)
-        console.log(filterData);
         let sum_list=[];
         filterData.forEach(element => {
             let obj={name:element.name,department:element.department,annualBonus:element.salary*0.1*element.experience}
             sum_list.push(obj);
-            
         });
-        console.log(sum_list)
-        const total=filterData.reduce(function(acc,curr){
+        let text='';
+        sum_list.forEach(element=>{
+            text+='<tr><td>'
+            text+='</td><td>'
+            text+=JSON.stringify(element.name);
+            text+='</td><td>'
+            text+=JSON.stringify(element.department);
+            text+='</td><td>'
+            text+=JSON.stringify(element.annualBonus);
+            text+='</td><td>'
+            text+='</td></tr>'
+        })
+        tablebody.innerHTML=text;
+        const expens=filterData.reduce(function(acc,curr){
             acc=acc+curr.salary;
             return acc;
         },0)
-        console.log(total);
+        bonus.innerText='annualBonus'
+        let text1=`TOTAL EXPENDITURE=${expens}`
+        total.innerText=text1;
+        
      }
+     
 
- display()
